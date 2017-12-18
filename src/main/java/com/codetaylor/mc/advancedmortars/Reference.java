56cc90ee559983aa574900d0d368dc31f2469964
Jason Taylor <jason@codetaylor.com>
2017-12-18 12:01:04 -0800

    fixed file placement

--- src/main/java/com/codetaylor/mc/advancedmortars/Reference.java
+++ src/main/java/com/codetaylor/mc/advancedmortars/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
   public static final String MOD_ID = "advancedmortars";
   public static final String VERSION = "@@VERSION@@";
   public static final String NAME = "Advanced Mortars";
-  public static final String DEPENDENCIES = "before:jei;after:crafttweaker";
+  public static final String DEPENDENCIES = "before:jei";
 
   public static final boolean IS_DEV = VERSION.equals("@@" + "VERSION" + "@@");
 
